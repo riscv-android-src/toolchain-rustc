c78e59221315ac280912d93c92c46d046ed6b172
CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>
2021-08-15 10:54:46 +0800

    add riscv support in libc rs also update the checksum file for libc-0.2.93
    Signed-off-by: CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>
    Change-Id: I10b32a6bdf34de0b369e4e8f1b634b5b98aae0ec

---
+++ vendor/rustc-ap-rustc_target/src/spec/riscv64_linux_android.rs
@@ -0,0 +1,14 @@
+use crate::spec::{CodeModel, Target, TargetOptions};
+
+pub fn target() -> Target {
+    let mut base = super::android_base::opts();
+    base.max_atomic_width = Some(64);
+    base.features = "+m,+a,+f,+d,+c".to_string();
+    Target {
+        llvm_target: "riscv64-linux-android".to_string(),
+        pointer_width: 64,
+        data_layout: "e-m:e-p:64:64-i64:64-i128:128-n64-S128".to_string(),
+        arch: "riscv64".to_string(),
+        options: TargetOptions { unsupported_abis: super::riscv_base::unsupported_abis(), ..base },
+    }
+}
