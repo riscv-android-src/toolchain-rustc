962edb20d600e9a4a815064a2eb9fca0801ea3fd
CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>
2021-08-17 17:46:03 +0800

    follow the riscv64gc_unknown_linux_gnu.rs to update the android one
    Signed-off-by: CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>
    Change-Id: Id5fef33548ebd684d30783f613525487e75d8ada

--- compiler/rustc_target/src/spec/riscv64_linux_android.rs
+++ compiler/rustc_target/src/spec/riscv64_linux_android.rs
@@ -4,11 +4,19 @@ pub fn target() -> Target {
     let mut base = super::android_base::opts();
     base.max_atomic_width = Some(64);
     base.features = "+m,+a,+f,+d,+c".to_string();
+    base.code_model = Some(CodeModel::Medium);
+    base.cpu = "generic-rv64".to_string();
+    base.features = "+m,+a,+f,+d,+c".to_string();
+    base.llvm_abiname = "lp64d".to_string();
+    base.max_atomic_width = Some(64);
     Target {
         llvm_target: "riscv64-linux-android".to_string(),
         pointer_width: 64,
         data_layout: "e-m:e-p:64:64-i64:64-i128:128-n64-S128".to_string(),
         arch: "riscv64".to_string(),
-        options: TargetOptions { unsupported_abis: super::riscv_base::unsupported_abis(), ..base },
+        options: TargetOptions {
+            unsupported_abis: super::riscv_base::unsupported_abis(),
+            ..base
+        },
     }
 }
